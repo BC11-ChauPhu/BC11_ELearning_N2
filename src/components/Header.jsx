e1b24a566b9f47e758f200df94a9c337e66ef902
Phu <chauphu159951@gmail.com>
2024-06-29 14:42:32 +0700

    update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useRef } from 'react'
 import { NavLink } from 'react-router-dom'
 import logo from '../img/logo-landing-1.png'
 
@@ -18,6 +18,23 @@ const Header = () => {
     }
   }, [])
 
+  const loginFormRef = useRef(null)
+  const signUpFormRef = useRef(null)
+
+  const handleClickOutside = (event) => {
+    if (event.target.className === 'active') {
+      if (loginFormRef.current && !loginFormRef.current.contains(event.target)) {
+        document.querySelector('#loginForm').classList.remove('active')
+      }
+      if (signUpFormRef.current && !signUpFormRef.current.contains(event.target)) {
+        document.querySelector('#signUpForm').classList.remove('active')
+      }
+    }
+  }
+
+  window.addEventListener('click', handleClickOutside)
+
+
 
   return (
     <>
@@ -32,37 +49,70 @@ const Header = () => {
               <NavLink className='navlinks-item' to='/'>Features</NavLink>
               <NavLink className='navlinks-item' to='/'>Explore</NavLink>
               <NavLink className='navlinks-item' to='/'>What's New</NavLink>
-              <NavLink className='navlinks-item' to='/'>Log In</NavLink>
-              <NavLink className='navlinks-item' to='/'>Sign Up</NavLink>
+              <NavLink className='navlinks-item' to='/' onClick={() => {
+                document.querySelector('#loginForm').classList.toggle('active')
+              }}>Log In</NavLink>
+              <NavLink className='navlinks-item' to='/' onClick={() => {
+                document.querySelector('#signUpForm').classList.toggle('active')
+              }}>Sign Up</NavLink>
             </div>
           </nav>
         </div>
       </header>
-      <section id="loginForm">
-          <div className="loginForm-content">
-            <div className="loginForm-title">
-              <h4>Create a new account</h4>
-            </div>
-            <div className="loginForm-form">
-              <form>
-                <div className="form-group">
-                  <input type="text" id="username" placeholder='Username' />
-                </div>
-                <div className="form-group">
-                  <input type="email" id="email" placeholder='Email'/>
-                </div>
-                <div className="form-group">
-                  <input type="password" placeholder='Password' />
-                </div>
-                <div className="form-group">
-                  <input type="password" placeholder='Repeat Password' />
-                </div>
-                <div className="form-group">
-                  <button type="submit">Sign Up</button>
-                </div>
-              </form>
-            </div>
+      <section id="loginForm" >
+        <div className="loginForm-content" ref={loginFormRef}>
+
+          <div className="loginForm-title">
+            <h4>Log in with your account</h4>
           </div>
+
+          <div className="loginForm-form">
+            <form>
+              <div className="form-group">
+                <input type="email" id="email" placeholder='Email' />
+              </div>
+              <div className="form-group">
+                <input type="password" placeholder='Password' />
+              </div>
+              <div className="form-group">
+                <button type="submit">LOG IN</button>
+              </div>
+            </form>
+          </div>
+          <p className='loginForm-link-bottom'>Don't you have an account yet? <a href=""><span>Sign up now</span></a></p>
+
+        </div>
+      </section>
+
+      <section id="signUpForm" >
+        <div className="signUp-content" ref={signUpFormRef}>
+
+          <div className="signUp-title">
+            <h4>Log in with your account</h4>
+          </div>
+
+          <div className="signUp-form">
+            <form>
+              <div className="form-group">
+                <input type="text" id="username" placeholder='Username' />
+              </div>
+              <div className="form-group">
+                <input type="email" id="email" placeholder='Email' />
+              </div>
+              <div className="form-group">
+                <input type="password" placeholder='Password' />
+              </div>
+              <div className="form-group">
+                <input type="password" placeholder='Repeat Password' />
+              </div>
+              <div className="form-group">
+                <button type="submit">Sign Up</button>
+              </div>
+            </form>
+          </div>
+          <p className='signUp-link-bottom'>Aldready have an account? <a href=""><span>Login now</span></a></p>
+
+        </div>
       </section>
     </>
 
